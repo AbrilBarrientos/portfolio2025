fcb12a09241c55bc88e1a67100b247fd215bdaf4
Abru <abrilpilarb@gmail.com>
2025-05-11 18:43:06 -0300

    act 11/5 18:43

--- js/proyectosProgram.js
+++ js/proyectosProgram.js
@@ -2,56 +2,56 @@ const proyectosProgramacion = [
     {
         titulo: "Federación Metropolitana de Voleibol",
         descripcion: "Página de noticias, torneo y reglamentos para la liga metropolitana de voleibol. Se pensaba complementar con PHP, pero por desacuerdos no se terminó el desarrollo",
-        img: "../img/program/fmv.PNG",
+        img: "img/program/fmv.PNG",
         link: "https://abrilbarrientos.github.io/fmV/",
         tags: ["JS", "HTML", "CSS"]
     },
     {
         titulo: "Buscador de series y detalles",
         descripcion: "Proyecto universitario ft. Alvarez-Giacomini para buscar titulos y detalles de series de tv. Desarrollado con API 'TVMAZE'",
-        img: "../img/program/buscador-shows.PNG",
+        img: "img/program/buscador-shows.PNG",
         link: "https://p1-awp-alvarez-barrientos-giacomini.netlify.app",
         tags: ["JS", "HTML", "CSS"]
     },
     {
         titulo: "Dashboard tareas",
         descripcion: "Dasboard para generar cards de tareas con etiquetas y fechas personalizadas, asignar prioridades y completar o eliminar del espacio.",
-        img: "../img/program/tareas-dashboard.PNG",
+        img: "img/program/tareas-dashboard.PNG",
         link: "https://dashboard-tareas.vercel.app",
         tags: ["JS", "HTML", "CSS"]
     },
     {
         titulo: "Tip Calculator",
         descripcion: "Proyecto de calculadora de propinas destinado a gastronomía",
-        img: "../img/program/tip-calculator.PNG",
+        img: "img/program/tip-calculator.PNG",
         link: "https://calculadora-propinas-swart.vercel.app",
         tags: ["JS", "HTML", "CSS"]
     },
     {
         titulo: "Aprender JS (en proceso)",
         descripcion: "Pagina de estudio y repaso para reforzar conocimientos de Javascript, contenido universitario y de cursos CODERHOUSE",
-        img: "../img/program/aprender-js.PNG",
+        img: "img/program/aprender-js.PNG",
         link: "https://abrilbarrientos.github.io/aprenderJavaScript/",
         tags: ["HTML", "CSS"]
     },
     {
         titulo: "CSS Smooth Animation Generator",
         descripcion: "Generador de animaciones suaves y dinámicas para elementos CSS",
-        img: "../img/program/css-anim-generator.PNG",
+        img: "img/program/css-anim-generator.PNG",
         link: "https://css-smoth-animations.vercel.app",
         tags: ["JS", "HTML", "CSS"]
     },
     {
         titulo: "Benefit ecommerce ft. Alvarez",
         descripcion: "Proyecto de tienda online para productos de maquillaje 'Benefit'",
-        img: "../img/program/benefit.PNG",
+        img: "img/program/benefit.PNG",
         link: "https://benefit-ecommerce.vercel.app",
         tags: ["JS", "HTML", "CSS"]
     },
     {
         titulo: "Log In / Sign Up | UX/UI test",
         descripcion: "Maquetado de vista para inicio de sesión y registro de cuenta para proyecto de página de citas.",
-        img: "../img/program/login-signup.PNG",
+        img: "img/program/login-signup.PNG",
         link: "https://log-in-test-sigma.vercel.app",
         tags: ["JS", "HTML", "CSS"]
     }
